16677a2e87914a4b928dae6cf5cff1343f27aa9f
emon2200 <procyonemon@gmail.com>
2025-11-29 23:56:20 +0600

    static

--- hospital/settings.py
+++ hospital/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
   
 ]
 CORS_ALLOW_ALL_ORIGINS = True
